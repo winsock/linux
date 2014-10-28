ff075f45ac349014086a2050f9070c7f25d45555
Thierry Reding <treding@nvidia.com>
2014-10-28 17:42:23 +0100

    WIP: drm/nouveau: Do not maintain cache for imported buffers
    The kernel oopses when dma_sync'ing for these buffers. The root cause
    for this hasn't been determined yet.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/nouveau/nouveau_bo.c
+++ drivers/gpu/drm/nouveau/nouveau_bo.c
@@ -465,6 +465,10 @@ nouveau_bo_sync_for_device(struct nouveau_bo *nvbo)
 	if (nvbo->force_coherent)
 		return;
 
+	/* trying to sync an imported buffer object will crash */
+	if (nvbo->bo.sg)
+		return;
+
 	for (i = 0; i < ttm_dma->ttm.num_pages; i++)
 		dma_sync_single_for_device(device->dev, ttm_dma->dma_address[i],
 					   PAGE_SIZE, DMA_TO_DEVICE);
@@ -483,6 +487,10 @@ nouveau_bo_sync_for_cpu(struct nouveau_bo *nvbo)
 
 	/* Don't waste time looping if the object is coherent */
 	if (nvbo->force_coherent)
+		return;
+
+	/* trying to sync an imported buffer object will crash */
+	if (nvbo->bo.sg)
 		return;
 
 	for (i = 0; i < ttm_dma->ttm.num_pages; i++)
