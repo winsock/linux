402b48c72a671c0fb2ebf5fafc5b21ede4d80a10
Thierry Reding <treding@nvidia.com>
2015-08-20 15:31:42 +0200

    drm/msm: Use unsigned int pipe consistently
    Use unsigned int pipe consistently to denote the CRTC index.
    
    Cc: Rob Clark <robdclark@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -116,7 +116,7 @@ u32 msm_readl(const void __iomem *addr)
 
 struct vblank_event {
 	struct list_head node;
-	int crtc_id;
+	unsigned int pipe;
 	bool enable;
 };
 
@@ -137,10 +137,10 @@ static void vblank_ctrl_worker(struct work_struct *work)
 
 		if (vbl_ev->enable)
 			kms->funcs->enable_vblank(kms,
-						priv->crtcs[vbl_ev->crtc_id]);
+						priv->crtcs[vbl_ev->pipe]);
 		else
 			kms->funcs->disable_vblank(kms,
-						priv->crtcs[vbl_ev->crtc_id]);
+						priv->crtcs[vbl_ev->pipe]);
 
 		kfree(vbl_ev);
 
@@ -151,7 +151,7 @@ static void vblank_ctrl_worker(struct work_struct *work)
 }
 
 static int vblank_ctrl_queue_work(struct msm_drm_private *priv,
-					int crtc_id, bool enable)
+				  unsigned int pipe, bool enable)
 {
 	struct msm_vblank_ctrl *vbl_ctrl = &priv->vblank_ctrl;
 	struct vblank_event *vbl_ev;
@@ -161,7 +161,7 @@ static int vblank_ctrl_queue_work(struct msm_drm_private *priv,
 	if (!vbl_ev)
 		return -ENOMEM;
 
-	vbl_ev->crtc_id = crtc_id;
+	vbl_ev->pipe = pipe;
 	vbl_ev->enable = enable;
 
 	spin_lock_irqsave(&vbl_ctrl->lock, flags);
